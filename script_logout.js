e435a9d3042e4876e7a74de5857001c6ce9dc064
Dhanshree1016 <147150112+Dhanshree1016@users.noreply.github.com>
2025-01-27 20:23:44 +0530

    a

--- script_logout.js
+++ script_logout.js
@@ -2,7 +2,7 @@ document.getElementById('logout-button').addEventListener('click', function () {
     const confirmLogout = confirm('Are you sure you want to logout?');
     if (confirmLogout) {
       // Redirect to the logout route or homepage
-      window.location.href = '/logout'; // Replace '/logout' with your logout URL
+      window.location.href = 'index.html'; // Replace '/logout' with your logout URL
     }
   });
   
